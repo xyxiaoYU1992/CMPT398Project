3fa9ee88fc70451e44067a56dad370796f7ee12a
xrdcrab <ruida.xie@usask.com>
2018-03-31 13:48:17 -0600

    Cluster initial setup

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
 // This is the main javascript file
+// Authro: Ruida Xie & Tong Wang
+
+// Set up geometric size
+var diameter = 940;
+var radius = diameter / 2;
+var innerRadius = radius - 270;
+
+
