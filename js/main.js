966826a8b3624db9ac4364931fadef84122575a6
xrdcrab <ruida.xie@usask.com>
2018-03-31 19:31:54 -0600

    finished index.html. added loom.js.:

--- js/main.js
+++ js/main.js
@@ -6,4 +6,14 @@ var diameter = 940;
 var radius = diameter / 2;
 var innerRadius = radius - 270;
 
+// Set up the data assessors
+var loom = d3.loom()
+    .value(function(d){ return d.appear})
+    .inner(function(d){ return d.name})
+    .outer(function(d){ return 0}) // TO DO: extract houses from characters, repeat characters allowed.
 
+// Set up appearence 
+loom.padAngle(0.05)
+    .widthInner(30)
+    .heightInner(20)
+    .emptyPerc(0.2)
